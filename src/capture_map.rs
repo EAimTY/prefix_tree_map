560a97fc51575193944feb42df5274df0b01d4b9
EAimTY <ea.imty@gmail.com>
2021-12-05 00:21:50 +0900

    implement `CaptureMap` for `HashMap`

--- src/capture_map.rs
+++ src/capture_map.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::{collections::{BTreeMap, HashMap}, hash::Hash};
 
 pub trait CaptureMap<W, E> {
     fn insert(&mut self, key: W, value: E);
@@ -9,6 +9,20 @@ impl<W, E> CaptureMap<W, E> for BTreeMap<W, E>
 where
     E: Clone + Ord,
     W: Clone + Ord,
+{
+    fn insert(&mut self, key: W, value: E) {
+        self.insert(key, value);
+    }
+
+    fn remove(&mut self, key: &W) {
+        self.remove(key);
+    }
+}
+
+impl<W, E> CaptureMap<W, E> for HashMap<W, E>
+where
+    E: Clone + Ord,
+    W: Clone + Hash + Ord,
 {
     fn insert(&mut self, key: W, value: E) {
         self.insert(key, value);
